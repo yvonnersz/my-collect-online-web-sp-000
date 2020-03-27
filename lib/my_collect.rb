741ff299ec6251bedeb70f8f11e09aee97f42d07
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-27 16:40:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 
 
 def my_collect(array)
-  array.split(" ").first
   i = 0
+  collection = []
   while i < array.length
     collection << yield(array[i])
-    i +=1
+    i += 1
   end
   collection
 end
